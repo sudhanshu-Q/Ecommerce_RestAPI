76b53c9a56fa01dde7b7b4aee3e55754a838550a
sudhanshu <sudhanshu.pat.kumar@gmail.com>
2021-06-18 13:41:34 +0530

    Creating get request for TC_2_Store

--- EcommerceRestApi/src/test/java/ecommerce/testing/testResponseValiadation/ResponseValidations.java
+++ EcommerceRestApi/src/test/java/ecommerce/testing/testResponseValiadation/ResponseValidations.java
@@ -5,6 +5,8 @@ package ecommerce.testing.testResponseValiadation;
 
 import org.apache.log4j.Logger;
 
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 
 /**
@@ -12,16 +14,48 @@ import io.restassured.response.Response;
  *
  */
 public class ResponseValidations {
-	
+
 	final static Logger log=Logger.getLogger(ResponseValidations.class);
-	
+
 	public void responseValidations(Response getResponse) {
-		
+
 		log.debug("..............Getting response.............");
 		log.info("Getting status code: "+getResponse.getStatusCode());
 		log.info("Getting status line in response: "+getResponse.getStatusLine());
 		log.info("Getting response body: "+getResponse.getBody());
 		log.info("Response header :"+getResponse.getHeaders().asList());
+		//capturing response headers 
+		Headers header = getResponse.headers();
+		for (Header allheader: header)
+		{
+			log.info("key :"+allheader.getName()+" | " +"value: "+allheader.getValue());
+			String xpoweredResponseHeader=header.get("x-powered-by").getValue();
+			if (allheader.getName().equals(xpoweredResponseHeader)) {
+				log.info(xpoweredResponseHeader.equals("Express")+" : "+xpoweredResponseHeader+" : Verifying | x-powered-by | Response header");
+			}
+			else {
+				log.error("Not verified : "+xpoweredResponseHeader);
+			}
+		}
+		//		String xpoweredResponseHeader=header.get("x-powered-by").getValue();
+		//		if (xpoweredResponseHeader.equals("Expressk")) {
+		//		log.info(xpoweredResponseHeader.equals("Express")+" : "+xpoweredResponseHeader+" : Verifying | x-powered-by | Response header");
+		//		}
+		//		else {
+		//			log.error("Not verified : "+xpoweredResponseHeader);
+		//		}
+		/**
+		 * {
+  "access-control-allow-origin": "*",
+  "allow": "GET,POST,PUT,PATCH,DELETE",
+  "content-encoding": "gzip",
+  "content-type": "application/json; charset=utf-8",
+  "date": "Fri, 18 Jun 2021 03:51:20 GMT",
+  "etag": "W/\"175b-ahK67GuMia7YzxPW6JneHQxymiA\"",
+  "vary": "Accept",
+  "x-powered-by": "Express"
+}
+		 **/
 	}
 
 }
---
+++ EcommerceRestApi/src/test/java/ecommerce/testing/testcases/TC_2_GetStores.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package ecommerce.testing.testcases;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import ecommerce.testing.testResponseValiadation.ResponseValidations;
+import ecommerce.testing.testUtilities.PropertiesFileLoad;
+import ecommerce.testing.teststeps.HttpMethods;
+import io.restassured.response.Response;
+
+/**
+ * @author sudhanshu
+ *
+ */
+public class TC_2_GetStores {
+
+	final static Logger log=Logger.getLogger(TC_1_GetProduct.class);
+	
+	@Test
+	public void initiating_TC_2_GetStore() throws IOException
+	{
+		Properties porpertiesRead=PropertiesFileLoad.propertyFileLoad("./EnvironmentConfigurations/Env.properties");
+		log.debug("Reading propery file :"+porpertiesRead.getProperty("GetStore"));
+		HttpMethods httpMethod=new HttpMethods(porpertiesRead);
+		log.debug("Requesting get http method request :"+httpMethod.toString());
+		Response responseGet=httpMethod.GetRequest("GetStore");
+		log.debug("Response captured for get request as Pretty string method:"+responseGet.asPrettyString());
+		ResponseValidations validateResponseGet=new ResponseValidations();
+		log.debug("capturing response");
+		validateResponseGet.responseValidations(responseGet);
+	}
+
+}
