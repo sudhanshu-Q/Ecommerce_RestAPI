dfa2aa5dc9be00735affec61fc8b37fce9cd0d44
sudhanshu-Q <61355968+sudhanshu-Q@users.noreply.github.com>
2021-06-20 18:14:33 +0530

    Commit for TC_3_GetServices

--- EcommerceRestApi/src/test/java/ecommerce/testing/testcases/TC_3_GetServices.java
+++ EcommerceRestApi/src/test/java/ecommerce/testing/testcases/TC_3_GetServices.java
@@ -19,24 +19,24 @@ import io.restassured.response.Response;
  * @author sudhanshu
  *
  */
-public class TC_2_GetStores {
-
-	final static Logger log=Logger.getLogger(TC_1_GetProduct.class);
+public class TC_3_GetServices {
+	
+	final static Logger log=Logger.getLogger(TC_3_GetServices.class);
 
 	@Test
-	public void initiating_TC_2_GetStore() throws IOException
+	public void initiating_TC_3_GetServices() throws IOException
 	{
-		Reporter.log("Starting TC_2_GetStore");
+		Reporter.log("Starting TC_3_GetServices");
 		Properties porpertiesRead=PropertiesFileLoad.propertyFileLoad("./EnvironmentConfigurations/Env.properties");
-		log.debug("Reading propery file :"+porpertiesRead.getProperty("GetStore"));
+		log.debug("Reading propery file :"+porpertiesRead.getProperty("GetServices"));
 		HttpMethods httpMethod=new HttpMethods(porpertiesRead);
 		log.debug("Requesting get http method request :"+httpMethod.toString());
-		Response responseGet=httpMethod.GetRequest("GetStore");
+		Response responseGet=httpMethod.GetRequest("GetServices");
 		log.debug("Response captured for get request as Pretty string method:"+responseGet.asPrettyString());
 		ResponseValidations validateResponseGet=new ResponseValidations();
 		log.debug("capturing response");
 		validateResponseGet.responseValidations(responseGet);
-		Reporter.log("Executed TC_2_GetStores successfully");
+		Reporter.log("Executed TC_3_GetServices successfully");
 	}
 
 }
